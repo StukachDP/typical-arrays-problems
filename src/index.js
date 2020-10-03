7f1337e83c35bb52b2036bdda5d22f3159bc1742
StukachDP <stukachdima06112000@gmail.com>
2020-10-04 00:02:51 +0300

    solution of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) === false || array.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let index = 0; index < array.length; index++) {
+        sum += array[index];
+    }
+    let avg = sum / array.length;
+    return avg;
 }
